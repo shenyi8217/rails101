923d326b9277701e13d2bbc4714badc6d8aff6b9
shenyi <shenyi8217@gmail.com>
2017-01-06 22:53:32 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,5 @@ class Group < ApplicationRecord
   has_many :posts
   validates :title, presence: true
   has_many :group_raltionships
-  has_many :members, through: :group_raltionships, source :user
+  has_many :members, through: :group_raltionships, source: :user
 end
